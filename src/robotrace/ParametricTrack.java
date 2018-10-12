14d2cc90526f3285720159b42103dbe711ee49bb
d.v.leijenhorst@gmail.com <d.v.leijenhorst@gmail.com>
2018-10-12 17:04:07 +0200

    Added basic tracks

--- src/robotrace/ParametricTrack.java
+++ src/robotrace/ParametricTrack.java
@@ -19,5 +19,13 @@ public class ParametricTrack extends RaceTrack {
                            28 * Math.PI * Math.cos(2 * Math.PI * t),
                            0);
     }
-    
+
+    @Override
+    protected Vector getNormal(double t) {
+        double length = Math.sqrt(Math.pow(28 * Math.PI * Math.cos(2 * Math.PI * t), 2) +
+                                  Math.pow(20 * Math.PI * Math.sin(2 * Math.PI * t), 2));
+        return new Vector(28 * Math.PI * Math.cos(2 * Math.PI * t) / length,
+                          20 * Math.PI * Math.sin(2 * Math.PI * t) / length,
+                          0);
+    }
 }
--- src/robotrace/RaceTrack.java
+++ src/robotrace/RaceTrack.java
@@ -27,7 +27,33 @@ abstract class RaceTrack {
      * Draws this track, based on the control points.
      */
     public void draw(GL2 gl, GLU glu, GLUT glut) {
+        final float N = 25;
+        Vector rem = Vector.O;
+        Vector remn = Vector.O;
 
+        for (int i = 0; i <= N; i++) {
+            Vector v = getPoint(i/N);
+            Vector n = getNormal(i/N);
+
+            if (rem != Vector.O && remn != Vector.O) {
+                gl.glColor3f(0f,255f,0f);
+                gl.glBegin(gl.GL_POLYGON);
+                    gl.glVertex3d(v.x, v.y, v.z);
+                    gl.glVertex3d(rem.x, rem.y, rem.z);
+                    gl.glVertex3d(rem.x + 2 * laneWidth * remn.x, rem.y + 2 * laneWidth * remn.y, rem.z);
+                    gl.glVertex3d(v.x + 2 * laneWidth * n.x, v.y + 2 * laneWidth * n.y, v.z);
+                gl.glEnd();
+                gl.glColor3f(0f,255f,255f);
+                gl.glBegin(gl.GL_POLYGON);
+                    gl.glVertex3d(v.x, v.y, v.z);
+                    gl.glVertex3d(rem.x, rem.y, rem.z);
+                    gl.glVertex3d(rem.x - 2 * laneWidth * remn.x, rem.y - 2 * laneWidth * remn.y, rem.z);
+                    gl.glVertex3d(v.x - 2 * laneWidth * n.x, v.y - 2 * laneWidth * n.y, v.z);
+                gl.glEnd();
+            }
+            rem = v;
+            remn = n;
+        }
     }
     
     /**
@@ -57,4 +83,6 @@ abstract class RaceTrack {
 
     // Returns a tangent on the test track at 0 <= t < 1.
     protected abstract Vector getTangent(double t);
+
+    protected abstract Vector getNormal(double t);
 }
